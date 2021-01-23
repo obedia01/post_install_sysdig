b98037540a656906dfafb32b1749c457326ec559
obedia01 <obedia01@hotmail.com>
2021-01-23 01:39:29 +0100

    final delivery

--- post_install_sysdig.sh
+++ post_install_sysdig.sh
@@ -1,21 +1,19 @@
-INSTANCE_NAME=exp-cloud-monitoring
-GUID=$(./get_ibm_instance_id.sh $INSTANCE_NAME)
+APIKEY=$1
+echo APIKEY:$APIKEY
 
-echo INSTANCE_NAME:$INSTANCE_NAME
+GUID=$2
 echo GUID:$GUID
 
-APIKEY=$1
 AUTH_TOKEN=$(./get_iam_token.sh $APIKEY)
 
-echo APIKEY:$APIKEY
 echo AUTH_TOKEN:$AUTH_TOKEN
 
-IBM_REGION=$2
+IBM_REGION=$3
 echo IBM_REGION:$IBM_REGION
 
-#./create_sysdig_team.sh team_OPS.json $IBM_REGION $GUID $AUTH_TOKEN
-#./create_sysdig_team.sh team_DEV.json $IBM_REGION $GUID $AUTH_TOKEN
-#./create_sysdig_team.sh team_BP2I.json $IBM_REGION $GUID $AUTH_TOKEN
+./create_sysdig_team.sh team_OPS.json $IBM_REGION $GUID $AUTH_TOKEN
+./create_sysdig_team.sh team_DEV.json $IBM_REGION $GUID $AUTH_TOKEN
+./create_sysdig_team.sh team_BP2I.json $IBM_REGION $GUID $AUTH_TOKEN
 
 TEAMID=$(./get_team_monitor_operations_id.sh $IBM_REGION $GUID $AUTH_TOKEN)
 echo TEAMID:$TEAMID
