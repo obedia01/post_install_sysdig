13bc9c029184c4ce2838e95072ea279aad112b3b
obedia01 <45194996+obedia01@users.noreply.github.com>
2021-01-23 01:51:30 +0100

    Add files via upload

---
+++ all_commands_together.sh
@@ -0,0 +1,25 @@
+ENV=$1
+TOKEN_SYSDIG_TOPIC=$2
+
+if [ -f "notificationChannel.json" ]; then
+	rm notificationChannel.json;echo "Fichier notificationChannel supprimé" 
+fi
+sed "s/@_TOKEN_SYSDIG_TOPIC_@/$TOKEN_SYSDIG_TOPIC/g" notificationChannel_$ENV.json > notificationChannel.json
+
+
+APIKEY=$3
+
+GUID=$4
+
+AUTH_TOKEN=$(curl -k -X POST 'https://iam.cloud.ibm.com/identity/token' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept:application/json' -d 'grant_type=urn:ibm:params:oauth:grant-type:apikey' -d 'apikey='$APIKEY \| jq -j ".access_token")
+
+IBM_REGION=$5
+
+curl -k -X POST https://$IBM_REGION.monitoring.cloud.ibm.com/api/teams -H 'Authorization: Bearer '$AUTH_TOKEN  -H 'IBMInstanceID: '$GUID   -H 'Content-Type: application/json' -d @team_OPS.json
+curl -k -X POST https://$IBM_REGION.monitoring.cloud.ibm.com/api/teams -H 'Authorization: Bearer '$AUTH_TOKEN  -H 'IBMInstanceID: '$GUID   -H 'Content-Type: application/json' -d @team_DEV.json
+curl -k -X POST https://$IBM_REGION.monitoring.cloud.ibm.com/api/teams -H 'Authorization: Bearer '$AUTH_TOKEN  -H 'IBMInstanceID: '$GUID   -H 'Content-Type: application/json' -d @team_BP2I.json
+
+TEAMID=$(curl -k -X GET https://$IBM_REGION.monitoring.cloud.ibm.com/api/teams -H 'Authorization: Bearer '$AUTH_TOKEN -H 'IBMInstanceID: '$GUID -H 'Content-Type: application/json' \| jq -c -S ".teams["0"].id")
+
+sed -i "s/@_TEAMID_@/$TEAMID/g" notificationChannel.json
+curl -k -X POST https://$IBM_REGION.monitoring.cloud.ibm.com/api/notificationChannels -H 'Authorization: Bearer '$AUTH_TOKEN -H 'Content-Type: application/json' -H 'IBMInstanceID: '$GUID -d @notificationChannel.json
