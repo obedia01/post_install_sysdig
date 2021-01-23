8d775de3da56947497275afec4117f71812d18d9
obedia01 <obedia01@hotmail.com>
2021-01-23 02:20:03 +0100

    final delivery #1

--- all_commands_together.sh
+++ all_commands_together.sh
@@ -11,15 +11,15 @@ APIKEY=$3
 
 GUID=$4
 
-AUTH_TOKEN=$(curl -k -X POST 'https://iam.cloud.ibm.com/identity/token' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept:application/json' -d 'grant_type=urn:ibm:params:oauth:grant-type:apikey' -d 'apikey='$APIKEY \| jq -j ".access_token")
+AUTH_TOKEN=$(curl -g -k -X POST 'https://iam.cloud.ibm.com/identity/token' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept:application/json' -d 'grant_type=urn:ibm:params:oauth:grant-type:apikey' -d 'apikey='$APIKEY | jq -j '.access_token')
 
 IBM_REGION=$5
 
-curl -k -X POST https://$IBM_REGION.monitoring.cloud.ibm.com/api/teams -H 'Authorization: Bearer '$AUTH_TOKEN  -H 'IBMInstanceID: '$GUID   -H 'Content-Type: application/json' -d @team_OPS.json
-curl -k -X POST https://$IBM_REGION.monitoring.cloud.ibm.com/api/teams -H 'Authorization: Bearer '$AUTH_TOKEN  -H 'IBMInstanceID: '$GUID   -H 'Content-Type: application/json' -d @team_DEV.json
-curl -k -X POST https://$IBM_REGION.monitoring.cloud.ibm.com/api/teams -H 'Authorization: Bearer '$AUTH_TOKEN  -H 'IBMInstanceID: '$GUID   -H 'Content-Type: application/json' -d @team_BP2I.json
+curl -g -k -X POST https://$IBM_REGION.monitoring.cloud.ibm.com/api/teams -H 'Authorization: Bearer '$AUTH_TOKEN  -H 'IBMInstanceID: '$GUID   -H 'Content-Type: application/json' -d @team_OPS.json
+curl -g -k -X POST https://$IBM_REGION.monitoring.cloud.ibm.com/api/teams -H 'Authorization: Bearer '$AUTH_TOKEN  -H 'IBMInstanceID: '$GUID   -H 'Content-Type: application/json' -d @team_DEV.json
+curl -g -k -X POST https://$IBM_REGION.monitoring.cloud.ibm.com/api/teams -H 'Authorization: Bearer '$AUTH_TOKEN  -H 'IBMInstanceID: '$GUID   -H 'Content-Type: application/json' -d @team_BP2I.json
 
-TEAMID=$(curl -k -X GET https://$IBM_REGION.monitoring.cloud.ibm.com/api/teams -H 'Authorization: Bearer '$AUTH_TOKEN -H 'IBMInstanceID: '$GUID -H 'Content-Type: application/json' \| jq -c -S ".teams["0"].id")
+TEAMID=$(curl -g -k -X GET https://$IBM_REGION.monitoring.cloud.ibm.com/api/teams -H 'Authorization: Bearer '$AUTH_TOKEN -H 'IBMInstanceID: '$GUID -H 'Content-Type: application/json' | jq -c -S ".teams["0"].id")
 
 sed -i "s/@_TEAMID_@/$TEAMID/g" notificationChannel.json
-curl -k -X POST https://$IBM_REGION.monitoring.cloud.ibm.com/api/notificationChannels -H 'Authorization: Bearer '$AUTH_TOKEN -H 'Content-Type: application/json' -H 'IBMInstanceID: '$GUID -d @notificationChannel.json
+curl -g -k -X POST https://$IBM_REGION.monitoring.cloud.ibm.com/api/notificationChannels -H 'Authorization: Bearer '$AUTH_TOKEN -H 'Content-Type: application/json' -H 'IBMInstanceID: '$GUID -d @notificationChannel.json
